08ca823dcd5f97b2c13af3caa8058ae9eb2b5ace
Titima Kasemsritanawat <titima.kp@gmail.com>
2024-08-25 17:36:12 +0700

    Answer Exercise #4

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,39 @@
 import "./App.css";
 import axios from "axios";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 
 function App() {
-  const [searchText, setSearchText] = useState("");
+  //const [searchText, setSearchText] = useState("");
   const [results, setResult] = useState([]);
 
-  useEffect (() => {
-    if(searchText){
-      findBook(searchText);
-  }
-  }, [searchText]);
+  const debounce = (func) => {
+    let timer;
+    return function (...args) {
+      const context = this;
+      if (timer) clearTimeout(timer);
+      timer = setTimeout(() => {
+        timer = null;
+        func.apply(context, args);
+      }, 500);
+    };
+  };
 
-  const handleSearch = (event) => {
-    setSearchText(event.target.value);
-  }
+
 
   const findBook = async (queryParameter) => {
+    if(queryParameter.length == 0){
+      queryParameter = null;
+    }
     const result = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=${queryParameter}`);
     setResult(result.data.items);  
   }
 
+  const optimizedFn = useCallback(debounce(findBook), []);
+
   return (
   <div className="App">
     <h2>Find a Book</h2>
-    <div><input type="text" onChange={handleSearch}></input></div>
+    <div><input type="text" onChange={(e) => optimizedFn(e.target.value)}></input></div>
     {results.length > 0 && (
         <>
           {results.map((result, index) => (
